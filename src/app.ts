cc46913e865731987550d2daf80e90277bb4b6aa
Bruno Torres <brunotorres@bemol.onmicrosoft.com>
2021-05-30 09:24:55 -0400

    feat: helmet and cors included and package.json repository adjust

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
+import helmet from 'helmet'
 import routes from './routes'
 
 dotenv.config()
 
 const app = express()
 
+app.use(cors())
+app.use(helmet())
 app.use(express.json())
-
-app.use(routes)
 app.use('/api', routes) // base endpoint
 
 export default app
